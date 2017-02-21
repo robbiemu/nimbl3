5a9a6ce732807e65099efc2318aa11cf60720b94
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-02-21 01:35:10 -0500

    0.9.6
    dummy data updated

--- nimbl3-technical-test/src/components/Dummy.vue
+++ nimbl3-technical-test/src/components/Dummy.vue
@@ -9,17 +9,24 @@
       <h1>Project highlights</h1>
       <ol>
         <li>well-rendered pc media</li>
-        <li>fully functional animation and search</li>
+        <li>fully functional animation and order lineitem search</li>
         <li>modular scss, components with vue's vuex2 flux architecture</li>
         <li>autoprefixer matching last 2 and ie >= 10</li>
-        <li>use of noimage_placeholder for missing nav</li>
-        <li>however, custom svg for search animation since asset not provided</li>
-        <li>and since poweredby image is not ready, rebuilt it in css/html</li>
         <li>Delivery float is dockable</li>
         <li>meaningful dummy data</li>
         <li>proper framework for wiring up to real API</li>
+        <li>tested with ~3MB of dummy records - remains fast!</li>
+      </ol>
+      <h1>Caveats</h1>
+      <ol>
+        <li>use of noimage_placeholder for missing nav</li>
+        <li>however, custom svg for search animation since asset not provided</li>
+        <li>and since poweredby image is not ready, rebuilt it in css/html</li>
         <li>rudamentary e2e tests</li>
         <li>rudamentary karma unit tests</li>
+        <li>mostly a weekend project, I had no time to look at this until the weekend.</li>
+        <li>incomplete bp-small changes, but you can see it is going along.</li>
+        <li>didn't understand the second search form</li>
       </ol>
     </div>
   </div>
